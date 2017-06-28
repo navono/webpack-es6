bf7d3673bf030e398ef2bd17e65664960b2b64d3
navono <navono007@gmail.com>
2017-06-28 16:01:02 +0800

    在css中增加svg文件，但是最终在html的svg的路径未定位到dist目录

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
  * @Author: Ping Qixing
  * @Date: 2017-06-28 15:20:50
  * @Last Modified by: Ping Qixing
- * @Last Modified time: 2017-06-28 15:42:18
+ * @Last Modified time: 2017-06-28 15:58:16
  * @Description
  */
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.svg'
 
 function component() {
   let element = document.createElement('div');
@@ -14,6 +15,14 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // 将图像添加到我们现有的 div。
+  let myIcon = new Image();
+
+  console.log(myIcon);
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
