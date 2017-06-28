0150af3ce99c7cace105104624cbfaf49a8cb795
navono <navono007@gmail.com>
2017-06-28 15:20:21 +0800

    更新文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,26 @@
  * @Author: Ping Qixing
  * @Date: 2017-06-07 15:25:54
  * @Last Modified by: Ping Qixing
- * @Last Modified time: 2017-06-07 16:10:18
+ * @Last Modified time: 2017-06-21 14:11:16
  */
+
+const path = require('path');
+const webpack = require('webpack');
 
 // CommonJS写法
 module.exports = {
+    context: path.resolve(__dirname, './src'),
     entry: {
-        bundle: __dirname + '/src/file.js'
+        // bundle: __dirname + '/src/file.js'
+        app: './app.js',
     },
     output: {
-        path: __dirname + '/dist',
-        filename: '[name].js'
+        // path: __dirname + '/dist',
+        // filename: '[name].js'
+        
+        path: path.resolve(__dirname, './dist/assets'),
+        filename: '[name].bundle.js',
+        publicPath: '/assets',
     },
     module: {
         // 大致意思是：所有的js文件，使用babel加载器来翻译一下
@@ -20,5 +29,8 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel?presents=es2015'
         }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './src'),
     }
 }
