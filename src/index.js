6944411546a1e78e8f974b671b2c273f2b030a51
navono <navono007@gmail.com>
2017-06-28 15:45:14 +0800

    增加资源管理的代码。演示如何将CSS打包

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 /*
  * @Author: Ping Qixing
  * @Date: 2017-06-28 15:20:50
- * @Last Modified by:   Ping Qixing
- * @Last Modified time: 2017-06-28 15:20:50
+ * @Last Modified by: Ping Qixing
+ * @Last Modified time: 2017-06-28 15:42:18
  * @Description
  */
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   let element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
   return element;
 }
 
