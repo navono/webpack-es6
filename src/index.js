522f45161d0ee48fe44441a0031cd555214611b2
navono <navono007@gmail.com>
2017-06-28 15:26:45 +0800

    从头开始构建学习webpack的过程。 在命令行中输入：webpack .\src\index.js dist\bundle.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+/*
+ * @Author: Ping Qixing
+ * @Date: 2017-06-28 15:20:50
+ * @Last Modified by:   Ping Qixing
+ * @Last Modified time: 2017-06-28 15:20:50
+ * @Description
+ */
+import _ from 'lodash';
+
 function component() {
   let element = document.createElement('div');
+
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   return element;
 }
